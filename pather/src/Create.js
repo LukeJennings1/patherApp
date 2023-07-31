7c239c4a9fcb6dda692ecea45e63e27150268d41
Luke Jennings <lukejennings123@gmail.com>
2023-07-31 23:52:31 +0100

    setup initial form for prompt sending

--- pather/src/Create.js
+++ pather/src/Create.js
@@ -4,14 +4,29 @@ import Header from './header/header.js';
 
 
 function Create() {
+
 return (
     <div>
         <Header />
-        hello
+        <form action = '' method = 'POST' id = 'promptFormSubmit'>
+            <input type='number' name = 'number-input' placeholder = 'number' id = 'number input'></input>
+            <input type='text' name = 'number-input' placeholder = 'title' id = 'text-input'></input>
+            <input type='number' name = 'number-input' placeholder = 'number' id = 'number input'></input>
+            <textarea name = 'prompt-input' placeholder='type a prompt'></textarea>
+            <button type = 'submit'>Submit</button>
+
+
+        </form>
     </div>
 )
 }
 
+window.addEventListener('load', () => {
+    const textInput = document.getElementById('text-input');
+    textInput.addEventListener('input', () => {console.log(textInput.value)});
+    console.log(textInput);
+    console.log('loaded')
+})
 
 
 export default Create;
